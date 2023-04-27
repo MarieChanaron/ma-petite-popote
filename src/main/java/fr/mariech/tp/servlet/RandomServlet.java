63d4355858ff27c871c58d5c9fcb5e61285b60c7
Marie Chanaron <chanaronmarie@gmail.com>
2023-04-27 22:05:38 +0200

    get random recipe

--- src/main/java/fr/mariech/tp/servlet/RandomServlet.java
+++ src/main/java/fr/mariech/tp/servlet/RandomServlet.java
@@ -7,15 +7,19 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "RecipeServlet", value = "/recipe")
-public class RecipeServlet extends HttpServlet {
+@WebServlet(name = "RandomServlet", value = "/random")
+public class RandomServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        long recipeId = Long.parseLong(request.getParameter("id"));
-        Recipe recipe = new RecipeService().fetchById(recipeId);
-        request.setAttribute("recipe", recipe);
-        request.getRequestDispatcher("/WEB-INF/recipe.jsp").forward(request, response);
+        List<Recipe> recipes = new RecipeService().fetchRecipes();
+        long nbRecipes = recipes.size();
+        double rand = Math.floor(Math.random() * nbRecipes);
+        int randInt = (int) rand;
+        long randomRecipeId = recipes.get(randInt).getId();
+        response.sendRedirect("/recipe?id=" + randomRecipeId);
     }
 
     @Override
