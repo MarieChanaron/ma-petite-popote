fa6117d1cb154f465f32b4b31b6bb0fa8c71333c
Marie Chanaron <chanaronmarie@gmail.com>
2023-04-27 18:33:45 +0200

    GET API done

--- src/main/java/fr/mariech/tp/resources/RecipeResource.java
+++ src/main/java/fr/mariech/tp/resources/RecipeResource.java
@@ -1,17 +1,27 @@
 package fr.mariech.tp.resources;
 
+import fr.mariech.tp.model.Recipe;
+import fr.mariech.tp.service.RecipeService;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.util.List;
+
 @Path("/recipes")
 public class RecipeResource {
 
+    public final RecipeService recipeService = new RecipeService();
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response fetchAll() {
-
+    public Response fetchAllRecipes() {
+        List<Recipe> allRecipes = recipeService.fetchRecipes();
+        return Response
+                .status(Response.Status.OK)
+                .entity(allRecipes)
+                .build();
     }
 }
